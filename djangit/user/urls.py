29d9ca2b066983df8b29e70e4d213b9f1c6a6f5a
Michael McKenzie <mikemckenzie987@gmail.com>
2019-07-03 00:11:45 -0400

    signin, signup, logout

--- djangit/user/urls.py
+++ djangit/user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib import admin
 
-from djangit.user.views import Profile
+from djangit.user.views import Homepage
 from djangit.user.models import DjangitUser
 
 admin.site.register(DjangitUser)
 
 urlpatterns = [
-    path('', Profile.as_view(), name='homepage'),
+    path('', Homepage.as_view(), name='homepage'),
 ]
