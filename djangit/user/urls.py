0d6b3e4513299aa82a4c5bff5fb10c2dfea07e6d
Michelle Hoelzer <hoelzermm@gmail.com>
2019-07-02 21:58:16 -0400

    forms added; urls and views updated with old code

--- djangit/user/urls.py
+++ djangit/user/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib import admin
 
-from djangit.user.views import Index
+from djangit.user.views import Index, SignUp, Login, Logout
 from djangit.user.models import DjangitUser
 
 admin.site.register(DjangitUser)
 
 urlpatterns = [
+    path('login/', Login.as_view()),
+    path('logout/', Logout.as_view()),
+    path('signup/', SignUp.as_view()),
     path('', Index.as_view(), name='homepage'),
 ]
--- djangit/user/views.py
+++ djangit/user/views.py
@@ -1,11 +1,75 @@
 from django.shortcuts import render
 from django.views import View
 
-from django.shortcuts import render
+from redjit.redjituser.forms import SignupForm, LoginForm
+from django.contrib.auth.models import User
+from redjit.redjituser.models import RedjitUser
+from django.contrib.auth import login, authenticate, logout
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+class SignUp(View):
+    html = 'signupform.html'
+
+    def get(self, request):
+        form = SignupForm()
+        return render(request, self.html, {'form': form})
+
+    def post(self, request):
+        form = None
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                # django 'User' built-in model
+                user = User.objects.create_user(
+                    username=data['username'],
+                    password=data['password'],
+                )
+                user.save()
+                # Actual Redjit user that can post and stuff
+                RedjitUser.objects.create(
+                    user=user,
+                    password=data['password']
+                )
+                login(request, user)
+                return HttpResponseRedirect(reverse('homepage'))
+
+
+class Login(View):
+    html = 'loginform.html'
+
+    def get(self, request):
+        form = LoginForm()
+        return render(request, self.html, {'form': form})
+
+    def post(self, request):
+        form = None
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user = authenticate(
+                    username=data['username'],
+                    password=data['password']
+                )
+                if user is not None:
+                    login(request, user)
+                    return HttpResponseRedirect(request.GET.get("next", "/"))
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse("homepage"))
+
+
 class Index(View):
     def get(self, request):
         html = 'index.html'
-        
+
         return render(request, html)
