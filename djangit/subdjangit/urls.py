c7ffcb503c2c1a72c14b150bb473f0fa23bfb9ca
Michelle Hoelzer <hoelzermm@gmail.com>
2019-07-09 12:16:54 -0400

    fixed url fir subdjangits

--- djangit/subdjangit/urls.py
+++ djangit/subdjangit/urls.py
@@ -9,5 +9,5 @@ admin.site.register(Subdjangit)
 urlpatterns = [
     path("subdjangits/", SubdjangitList.as_view(), name="subdjangits"),
     path("createsubdjangit/", CreateSubdjangit.as_view(), name='createsubdjangit'),
-    path("<str:title>/", SingleSubdjangit.as_view(), name="singlesubdjangit")
+    path("<str:Subdjangit.title>/", SingleSubdjangit.as_view(), name="singlesubdjangit")
 ]
