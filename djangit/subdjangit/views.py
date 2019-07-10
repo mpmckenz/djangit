67b43f3601eeb4bfb8512b779d9470b53cebe523
Jennifer Browning <browningjen@yahoo.com>
2019-07-10 14:50:27 -0400

    erased changes to repull having issues with sqllite

--- djangit/subdjangit/views.py
+++ djangit/subdjangit/views.py
@@ -1,6 +1,7 @@
+
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
-from djangit.post.form import PostForm
+
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -9,36 +10,14 @@ from djangit.user.models import DjangitUser
 from djangit.subdjangit.models import Subdjangit
 from djangit.subdjangit.forms import SubdjangitForm
 
+
 class SubdjangitList(View):
     """View to see all of the subdjangits"""
 
-    form_class = PostForm
-
     def get(self, request):
-        request = {}
-        form = self.form_class()
         html = "subdjangit.html"
-        sub = Subdjangit.objects.all().order_by("title")
-        posts = sort_posts(Post.objects.filter(subdangit=sub).all())
-        response.update({"sub": sub, "form": form,
-                        "posts": posts, })
-        return render(request, html, response)
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        toggle_post_upvotes(request)
-        toggle_post_downvotes(request)
-        if form.is_valid():
-            data = form.cleaned_data
-            user = DjangitUser.objects.get(pk=request.user.djangituser.pk)
-            Posts.object.create(
-                user = data['user'],
-                title = data['title'],
-                body = data['body'],
-                subdjangit = Subdjangit.objects.filter(title=subdjangit).first()
-
-            )
-        return HttpResponseRedirect(reverse('subdangit', kwargs={"subdjangit": subdjangit}))
+        subdjangits = Subdjangit.objects.all().order_by("title")
+        return render(request, html, {"subdjangits": subdjangits})
 
 
 class SingleSubdjangit(View):
@@ -51,12 +30,7 @@ class SingleSubdjangit(View):
         return render(request, html, {"subdjangit": subdjangit})
 
 
-
-
-
 class CreateSubdjangit(View):
-    """Create a new community"""
-
     def get(self, request):
         html = "createSubdjangitform.html"
         form = SubdjangitForm()
