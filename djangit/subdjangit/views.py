e510a57c78b6128c7d3093119f1268daf012af92
Michael McKenzie <mikemckenzie987@gmail.com>
2019-07-11 10:12:51 -0400

    create subdjangits

--- djangit/subdjangit/views.py
+++ djangit/subdjangit/views.py
@@ -22,9 +22,9 @@ class SubdjangitList(View):
 class SingleSubdjangit(View):
     """View for one subdjangit"""
 
-    def get(self, request, title):
+    def get(self, request, url):
         html = "singleSubdjangit.html"
-        subdjangit = Subdjangit.objects.filter(title=title).first()
+        subdjangit = Subdjangit.objects.filter(url=url).first()
         # do as pass in for the count of the subscribers
         return render(request, html, {"subdjangit": subdjangit})
 
@@ -41,22 +41,23 @@ class CreateSubdjangit(View):
         form = SubdjangitForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            if Subdjangit.objects.filter(title=data['title']):
-                return HttpResponseRedirect('r/{}/'.format(data['title']))
+            if Subdjangit.objects.filter(url=url):
+                return HttpResponseRedirect('r/{}/'.format(data['url']))
             else:
-                Subdjangit.objects.create(
-                    moderator=request.user.djangituser,
-                    title=data["title"],
-                    about=data["about"],
-                )
-                return HttpResponseRedirect('/')
+                if " " not in data['url']:
+                    Subdjangit.objects.create(
+                        moderator=request.user.djangituser,
+                        url=data['url'],
+                        title=data["title"],
+                        about=data["about"],
+                    )
+                    return HttpResponseRedirect('/')
 
 
 class DeleteSubdjangit(View):
     """Deletes subdjangit if logged in user is moderator"""
 
     def delete(self, request, subdjangit):
-
         subdjangit = Subdjangit.objects.filter(title=subdjangit)
         subdjangit.delete()
         return HttpResponseRedirect('/')
