12ad5d269bc5b2b86f73e38ebfe6baeaf5eebc34
Michael McKenzie <mikemckenzie987@gmail.com>
2019-07-11 14:33:48 -0400

    toggle subscription from subdjangit page

--- djangit/user/urls.py
+++ djangit/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib import admin
 
-from djangit.user.views import Homepage, ViewSpecificUserHomepage
+from djangit.user.views import Homepage, ViewSpecificUserHomepage, AllUsers, ToggleSubscription
 from djangit.user.models import DjangitUser
 
 admin.site.register(DjangitUser)
@@ -11,4 +11,6 @@ urlpatterns = [
     # could add u/ or user/ for users; str in urls; slug; separate endpoints
     path('user/<str:username>/', ViewSpecificUserHomepage.as_view()),
     # path('<str:subdjangit>/', ViewSpecificSubdjangit.as_view()),
+    path('allusers/', AllUsers.as_view(), name='allusers'),
+    path("subscribe/<str:url>/", ToggleSubscription.as_view()),
 ]
