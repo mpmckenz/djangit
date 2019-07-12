72bc8ac4165f25dfbd988094ed5299dd508bdedc
Jennifer Browning <browningjen@yahoo.com>
2019-07-12 10:56:55 -0400

    nothing changed

--- djangit/post/models.py
+++ djangit/post/models.py
@@ -5,7 +5,7 @@ from djangit.user.models import DjangitUser
 class Post(models.Model):
     user = models.ForeignKey(DjangitUser, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=150, default='')
-    body = models.CharField(max_length=500, default='')
+    body = models.TextField(max_length=500, default='')
     date_created = models.DateTimeField(auto_now_add=True)
     url = models.URLField(max_length=200)
     upvotes = models.IntegerField(default=0)
