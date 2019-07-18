e8093d992cc5bd275e8571bc4904dcae87da6a20
Jennifer Browning <browningjen@yahoo.com>
2019-07-18 18:41:39 -0400

    fixed double code on homepage/added name to post

--- djangit/post/form.py
+++ djangit/post/form.py
@@ -3,6 +3,8 @@ from djangit.post.models import Post
 
 
 class PostForm(forms.Form):
+    name = forms.CharField(max_length=30)
     title = forms.CharField(max_length=30)
     body = forms.CharField(widget=forms.Textarea)
+    
    
